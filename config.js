8dab9de1a906f16db8f106343938c4967946775e
SAUKI52 <117531177+SAUKI52@users.noreply.github.com>
2022-11-25 14:08:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -49,12 +49,12 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
 
-global.sig = 'https://instagram.com/_ctzhid' // ig
-global.sgh = 'https://github.com/WH-MODS-BOT/' // github
-global.sgc = 'https://chat.whatsapp.com/FQkbyTEfvES3D363uxMEFOI' // group whatsapp
-global.sdc = 'https://www.discord.com'//discord
+global.sig = '-' // ig
+global.sgh = '-' // github
+global.sgc = 'https://chat.whatsapp.com/LZYqzz7xXnt1BjM0NM1Xx9' // group whatsapp
+global.sdc = '-'//discord
 global.snh = 'https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow' // my youtube channel
-global.sfb = 'https://www.facebook.com/' // facebook
+global.sfb = '-' // facebook
 global.syt = 'https://www.youtube.com/' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
 
 /*============== PAYMENT ==============*/
